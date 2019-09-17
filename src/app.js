ca6d42d8e9cd4c800c0824201ab331b6178e4645
KA Goodreau <goodreau.kath@gmail.com>
2019-09-16 19:32:33 -0500

    Added use statements to app.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
+const {NODE_ENV} = require('/.config')
+const validateBearerToken = require('./validate-bearer-token')
+const bookmarksRouter = require('./bookmarks/bookmarks-router')
+const errorHandler = require('./errorHandler')
 
 const app = express()
 
@@ -13,5 +17,15 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
+
+app.use(validateBearerToken)
+app.use(bookmarksRouter)
+
+app.get('/', (req, res) => {
+    res
+        .send('Hello World!')
+})
+
+app.use(errorHandler)
 
 module.exports = app
